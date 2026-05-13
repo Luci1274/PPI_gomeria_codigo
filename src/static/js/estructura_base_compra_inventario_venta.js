f1d010c37522ba44440dc652f533f1496636ec57
Luciano <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2026-05-13 19:38:21 -0300

    feat: agregar botón de carrito y mejorar la barra lateral derecha

--- src/static/js/estructura_base_compra_inventario_venta.js
+++ src/static/js/estructura_base_compra_inventario_venta.js
@@ -1,4 +1,13 @@
-
 // Fecha en la barra lateral derecha
 const fecha = document.getElementById('fecha');
 fecha.textContent = new Date().toLocaleDateString();
+
+
+// Barra lateral derecha mobile
+
+const botonCarrito = document.getElementById('boton_carrito');
+const barraDerecha = document.getElementById('barra_derecha');
+
+botonCarrito.addEventListener('click', () => {
+    barraDerecha.classList.toggle('abierta_derecha');
+});
