02f8e10463d648f909d2d5214752a91bc92b44e8
Luciano <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2026-08-04 17:48:58 -0300

    actualizar consulta en sql_leer_productos para incluir idproducto_servicio

--- src/modulos/comandos_db/comandos_db_productos.py
+++ src/modulos/comandos_db/comandos_db_productos.py
@@ -1,8 +1,15 @@
 from modulos.conexion import conectar_db
-#---------------------------------------
+#----------------------------------------------------------------------------
 def sql_leer_productos():
     """La funcion lee los productos en la db y los devuelve"""
     conexion = conectar_db()
     cursor = conexion.cursor()
     
-    cursor.execute("SELECT p.nombre, p.tipo, imagen_producto, s.cantidad FROM producto_servicio AS p JOIN stock_actual AS s   ON p.idproducto_servicio = s.idstock_actual")
+    cursor.execute("SELECT p.idproducto_servicio, p.nombre, p.tipo, imagen_producto, s.cantidad FROM producto_servicio AS p JOIN stock_actual AS s   ON p.idproducto_servicio = s.idstock_actual")
+    listado_productos = cursor.fetchall()
+    
+    cursor.close()
+    conexion.close()
+    return listado_productos
+#----------------------------------------------------------------------------
+
