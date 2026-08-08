5f00e76863d8fda16426a9b32868db72846462f1
Luciano <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2026-08-08 18:54:15 -0300

    agregado: implementación de funciones para actualizar y eliminar productos en la base de datos

--- src/modulos/comandos_db/comandos_db_productos.py
+++ src/modulos/comandos_db/comandos_db_productos.py
@@ -105,5 +105,51 @@ def sql_leer_tipos():
     finally:
         conexion.close()
 #----------------------------------------------------------------------------
-def sql_actualizar_producto():
+def sql_actualizar_producto(id, datos):
+    """Actualiza los datos del producto """
+    conexion = conectar_db()
+    try:
+        with conexion.cursor() as cursor:
+            sql = """UPADATE producto_servicio SET
+                nombre = %s,
+                tipo = %s,
+                marca = %s,
+                medidas = %s,
+                imagen_producto = %s,
+                cantidad_actual = %s,
+                cantidad_minima = %s,
+                precio = %s
+                WHERE idproducto_servicio = %s;"""
+            cursor.execute(sql, (datos.get("nombre"), datos.get("tipo"), datos.get("marca"), datos.get("medidas"), datos.get("imagen_producto"), datos.get("cantidad_actual"), datos.get("cantidad_minima"), datos.get("precio"), id))
+            conexion.commit()
+        return cursor.rowcount > 0
     
+    except pymysql.MySQLError as e:
+        conexion.rollback()
+        print(f"Error al modificar el producto/servicio: {id}, el error es: {e} ")
+        return False
+    finally:
+        conexion.close()
+
+#----------------------------------------------------------------------------
+def sql_eliminar_producto(id):
+    """Elimina el producto de forma logica, no de db"""
+    conexion = conectar_db()
+    try:
+        with conexion.cursor() as cursor:
+            sql = """ UPDATE producto_servicio SET
+                activo = 0 
+                WHERE idproducto_servicio = %s;"""
+            cursor.execute(sql, (id))
+        conexion.commit()
+        return cursor.rowcount > 0
+    
+    except pymysql.MySQLError as e:
+        conexion.rollback()
+        print(f"Error al desactivar el producto: {id}, error: {e}")
+        return False
+    
+    finally:
+        conexion.close()
+        
+        
