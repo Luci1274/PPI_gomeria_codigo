88ce5996c674e00f6c49c740b5bfadfad265edb9
Michaa04 <michael.zarza@alumnos.ispi4038.edu.ar>
2026-09-08 16:52:37 -0300

    Agrego cambios

--- src/static/js/open_modal.js
+++ src/static/js/open_modal.js
@@ -1,10 +1,9 @@
-// Captura de elementos
-const modal = document.getElementById('modal-proveedor');
-const btnNuevo = document.getElementById('btn-nuevo-proveedor');
+const modal = document.getElementById('modal-overlay');
+const btnNuevo = document.getElementById('btn-open-modal');
 const btnCerrar = document.getElementById('btn-cerrar-modal');
 const btnCancelar = document.getElementById('btn-cancelar-modal');
 
-// Abrir modal al hacer clic en "Nuevo Proveedor"
+// Abrir modal al hacer clic en "Nuevo ..."
 btnNuevo.addEventListener('click', () => {
 modal.classList.add('activo');
 });
