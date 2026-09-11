638459028d7a9fbd6a83e26cc751049b2c51d5b5
Luciano <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2026-09-11 19:49:22 -0300

    Refactor la conexión a la base de datos eliminando el manejo de excepciones innecesario

--- src/app.py
+++ src/app.py
@@ -206,4 +206,3 @@ def eliminar_cliente(id):
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
--- src/config.py
+++ src/config.py
@@ -35,25 +35,14 @@ class Config:
     # 2. Métodos de conexión
     @staticmethod
     def conectar_db():
-        try:
-            return pymysql.connect(
-                host=os.environ.get("DB_HOST"),
-                user=os.environ.get("DB_USER"),
-                password=os.environ.get("DB_PASSWORD"),
-                database=os.environ.get("DB_NAME"),
-                port=int(os.environ.get("DB_PORT", 3306)),
-                connect_timeout=3,
-                cursorclass=pymysql.cursors.DictCursor
-            )
-        except:
-            return pymysql.connect(
-                host="localhost",
-                user="luci",
-                password="1274",
-                database="gomeria",
-                port=3306,
-                cursorclass=pymysql.cursors.DictCursor
-            )
+        return pymysql.connect(
+            host=os.environ.get("DB_HOST"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("DB_NAME"),
+            port=int(os.environ.get("DB_PORT", 3306)),
+            cursorclass=pymysql.cursors.DictCursor
+        )
 
     @staticmethod
     def conectar_cloudinary():
